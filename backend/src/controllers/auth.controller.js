ff7d54d92c5dedc8c66d5069c1c56dfd598abe46
Utkarsh Chaudhari <utkarshchaudhary483@gmail.com>
2025-07-06 21:18:51 +0530

    backend work

---
+++ backend/src/controllers/auth.controller.js
@@ -0,0 +1,11 @@
+export const signup = (req, res) => {
+  res.send("Signup controller working");
+};
+
+export const login = (req, res) => {
+  res.send("Login controller working");
+};
+
+export const logout = (req, res) => {
+  res.send("Logout controller working");
+};
---
+++ backend/src/server.js
@@ -0,0 +1,22 @@
+import express from "express";
+import dotenv from "dotenv";
+import authRoutes from "./route/auth.route.js";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(express.json()); // To parse JSON request bodies
+
+// Routes
+app.use("/api/auth", authRoutes);
+
+// Catch-all 404 route
+app.use((req, res) => {
+  res.status(404).json({ message: "Route not found" });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port number: ${PORT}`);
+});

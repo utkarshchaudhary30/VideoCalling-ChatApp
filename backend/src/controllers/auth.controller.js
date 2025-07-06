560a56159e4efa6902968ba2467fd59339b6f506
Utkarsh Chaudhari <utkarshchaudhary483@gmail.com>
2025-07-07 00:52:11 +0530

    feat: added user model with Schema definition

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,12 +1,13 @@
-export const signup = (req, res) => {
-  res.send("Signup controller working");
+export const signup = async (req, res) => {
+    const{email,password,fullName}=req.body;
+    
 };
 
-export const login = (req, res) => {
+export const login =async (req, res) => {
   res.send("Login controller working");
 };
 
-export const logout = (req, res) => {
+export const logout =async (req, res) => {
   res.send("Logout controller working");
 };
 

bc0c588bd5e4973de9c92378d16133e28c80175f
Utkarsh Chaudhari <utkarshchaudhary483@gmail.com>
2025-07-08 14:19:54 +0530

    install tailwind css - create frontend pages

---
+++ frontend/src/pages/ChatPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ChatPage = () => {
+  return (
+    <div>
+      chat krogeeee
+    </div>
+  )
+}
+
+export default ChatPage
---
+++ frontend/src/pages/HomePage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const HomePage = () => {
+  return (
+    <div>
+      HomePage hi hiiiaaa
+    </div>
+  )
+}
+
+export default HomePage

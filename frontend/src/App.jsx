21ab1dfbce8f6bafbd5c963dee07681ba87fa4c5
Utkarsh Chaudhari <utkarshchaudhary483@gmail.com>
2025-07-12 00:52:02 +0530

    implement chat-page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -63,8 +63,29 @@ const App = () => {
             )
           }
         />
-        <Route path='/call' element={isAuthenticated?<CallPage />:<Navigate to="/login"/>} />
-        <Route path='/chat' element={isAuthenticated?<ChatPage />:<Navigate to="/login"/>} />
+         <Route
+          path="/call/:id"
+          element={
+            isAuthenticated && isOnboarded ? (
+              <CallPage />
+            ) : (
+              <Navigate to={!isAuthenticated ? "/login" : "/onboarding"} />
+            )
+          }
+        />
+
+        <Route
+          path="/chat/:id"
+          element={
+            isAuthenticated && isOnboarded ? (
+              <Layout showSidebar={false}>
+                <ChatPage />
+              </Layout>
+            ) : (
+              <Navigate to={!isAuthenticated ? "/login" : "/onboarding"} />
+            )
+          }
+        />
           <Route
           path="/onboarding"
           element={

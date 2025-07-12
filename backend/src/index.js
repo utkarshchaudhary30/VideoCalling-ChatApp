c6e1b8319696a6f98006044d1865cdf59b07bd41
Utkarsh Chaudhari <utkarshchaudhary483@gmail.com>
2025-07-12 23:26:50 +0530

    final commit

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,6 @@
 import express from "express";
 import cors from "cors";
-
+import path from "path";
 import dotenv from "dotenv";
 import authRoutes from "./route/auth.route.js";
 import userRoutes from "./route/user.route.js";
@@ -12,7 +12,7 @@ dotenv.config();
 
 const app = express(); 
 const PORT = process.env.PORT || 5000;
-
+const __dirname=path.resolve();
 app.use(cors({
    origin:"http://localhost:5173",
    credentials:true
@@ -26,10 +26,12 @@ app.use("/api/chat",chatRoutes);
 
 
 // Catch-all 404 route
-app.use((req, res) => {
-  res.status(404).json({ message: "Route not found" });
-});
-
+if(process.env.NODE_ENV==="production"){
+  app.use(express.static(path.join(__dirname,"../frontend/dist")));
+  app.get("*",(req,res)=>{
+    res.sendFile(path.join(__dirname,"../frontend","dist","index.html"));
+  })
+}
 
 app.listen(PORT, () => {
   console.log(`Server is running on port number: ${PORT}`);
